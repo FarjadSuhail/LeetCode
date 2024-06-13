2cd63ba7814695e9c21d8ddadd15475bdd2ff6bf
Farjad Suhail <56956704+FarjadSuhail@users.noreply.github.com>
2024-06-13 14:05:03 +0200

    Time: 126 ms (8.18%), Space: 52.5 MB (34.31%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+
+    for(i = 0; i < nums.length - 1; i++) {
+      if(i > nums.length - 1) {
+        break;
+      }
+      if (nums[i] === nums[i + 1]) {
+        nums.splice(i, 1);
+        i--;
+      }
+    }
+    return nums.length;
+};
